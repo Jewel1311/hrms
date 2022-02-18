5b82d90e9f98e1ae6d4517071ee3b49a7d57e695
Jewel1311 <jewelmathew13@gmail.com>
2022-02-18 23:10:45 +0530

    pagination added

--- applicants/views.py
+++ applicants/views.py
@@ -1,6 +1,17 @@
+from queue import Empty
 from django.shortcuts import render
+from django.core.paginator import Paginator,EmptyPage
 from base.models import Jobs
 
 def jobs(request):
-    jobs=Jobs.objects.all()
-    return render(request, 'applicant/jobview.html',{'jobs': jobs})
+    jobs = Jobs.objects.all()
+    
+    p = Paginator(jobs,1)  # second argument is no of items to be displayed
+   
+    page_num = request.GET.get('page',1 ) #get the page no by url  and 1 is default
+    try:
+       page = p.page(page_num)
+    except EmptyPage:
+       page = p.page(1)
+
+    return render(request, 'applicant/jobview.html',{'jobs': page})
