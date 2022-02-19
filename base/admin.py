c51c43ae0eecbf82e456e83e3b80fcf96995ed21
Jewel1311 <jewelmathew13@gmail.com>
2022-02-19 22:56:50 +0530

    category to department

--- base/admin.py
+++ base/admin.py
@@ -1,14 +1,14 @@
 from tabnanny import verbose
 from django.contrib import admin
-from .models import Category, Jobs   
+from .models import Department, Jobs   
 
 
 class JobsAdmin(admin.ModelAdmin):
     ordering = ['-posted_on']
-    search_fields = ('job_title','location','category','posted_on','withdraw_date')
-    list_filter = ('job_title','location','category')
-    list_display = ('job_title','location','category','posted_on','withdraw_date' )
+    search_fields = ('job_title','location','department','posted_on','withdraw_date')
+    list_filter = ('job_title','location','department')
+    list_display = ('job_title','location','department','posted_on','withdraw_date' )
 
-admin.site.register(Category)
-admin.site.register(Jobs, JobsAdmin)
+admin.site.register(Department)
+admin.site.register(Jobs,JobsAdmin)
 
--- base/models.py
+++ base/models.py
@@ -2,15 +2,15 @@ from unicodedata import category
 from django.db import models
 
 
-class Category(models.Model):
-    category = models.CharField(max_length=60)
+class Department(models.Model):
+    department_name = models.CharField(max_length=60)
 
     class Meta:
-        verbose_name_plural = 'Category'
+        verbose_name_plural = 'Department'
 
 
     def __str__(self):
-        return self.category
+        return self.department_name
 
 
 class Jobs(models.Model):
@@ -20,7 +20,7 @@ class Jobs(models.Model):
     location = models.CharField(max_length=60)
     posted_on = models.DateField(auto_now_add=True)
     withdraw_date = models.DateField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
     
     class Meta:
         verbose_name_plural = 'Jobs'
