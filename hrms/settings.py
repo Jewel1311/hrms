0634d4149612c3bbef4299dc74ce2dccb6435f4f
Jewel <84492029+Jewel1311@users.noreply.github.com>
2022-07-13 06:47:33 +0530

    Update settings.py

--- hrms/settings.py
+++ hrms/settings.py
@@ -156,5 +156,5 @@ LOGIN_URL = 'login'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_USE_TLS = True
 # EMAIL_PORT = 587
-# EMAIL_HOST_USER = "jewelmathew13@gmail.com"
-# EMAIL_HOST_PASSWORD = "sofvdxsyeljfiwlo"
+# EMAIL_HOST_USER = ""
+# EMAIL_HOST_PASSWORD = ""
